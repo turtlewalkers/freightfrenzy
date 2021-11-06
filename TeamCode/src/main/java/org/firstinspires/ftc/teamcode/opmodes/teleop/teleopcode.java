a04a50a1af5655eb96168ebb71645b357c2398cd
saa938 <83087832+saa938@users.noreply.github.com>
2021-11-06 14:46:01 -0700

    teleop import carousel
    made teleop
    imported more stuff in autonomous
    changed the name of carousel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/teleopcode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/teleopcode.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
 import org.firstinspires.ftc.teamcode.robot.TurtleRobot;
 
 // Controls:
